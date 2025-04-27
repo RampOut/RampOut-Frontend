089fdff3ea253379a7e52237981c47ee759ff30b
Araceli <aracelirusa@gmail.com>
2025-04-26 19:23:55 -0700

    feat: Creación y estilización de página de inicio para poder hacer login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,22 @@
 function Login() {
-  return(<>
-          <h1 className="text-bg-danger">Login</h1>
-        </>);
+  return (
+    <>
+      <div className="background-login cointainer d-flex justify-content-center align-content-center align-items-center">
+        <div className="d-flex flex-column align-items-center top-50 start-50 box-shadow"  style={{ background: "linear-gradient(#e0e0e0, #c0c0c0)", width: "100vh", height:"60vh" }}>
+          <div className="d-flex flex-column align-items-center justify-content-center w-100 h-100">
+          <button
+            className="btn col-10 m-2 p-2"
+            onClick={() => (window.location.href = "profesor")}>
+            SOY UN PROFESOR
+          </button>
+          <button className="btn col-10 m-2 p-2" onClick={() => (window.location.href = "equipo")}>
+            SOY UN ALUMNO
+          </button>
+          </div>
+          </div>
+        </div>
+    </>
+  );
 }
 
 export default Login;
