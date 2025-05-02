74facfed0019e92dc7efee21f14940bf92c5058a
Araceli <aracelirusa@gmail.com>
2025-05-01 20:30:10 -0700

    logout in profesor

--- src/pages/ConfigProfesor.tsx
+++ src/pages/ConfigProfesor.tsx
@@ -1,12 +1,31 @@
+import { logOut } from "../api/ProfesorAPI";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth} from "../auth/AuthProvider";
+import "../styles/index.css"; // Importamos los estilos
+
 interface Props {};
 
 const ConfigProfesor = () => {
+    const auth = useAuth();
+    const navigate = useNavigate();
+
     return(
         <>
         <h1>Configuración del Profesor</h1>
-        <p>Ir a Juego</p>
-        <p>Ver Estadísticas de los estudiantes???</p>
-        <p>Log Out</p>
+        <Link to="/game">Ir al Juego</Link>
+        <button 
+                    className="rampout-btn logout-btn" 
+                    onClick={()=>{
+                      try{
+                        logOut();
+                        alert("Logging Out");
+                        auth.setIsAuthenticated(false);
+                      }
+                      catch(e){console.log(e)}
+                    }}
+                  >
+                    LOG OUT
+        </button>
         </>
     )
 }
